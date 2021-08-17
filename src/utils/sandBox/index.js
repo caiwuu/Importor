5248ae51809870b1477fc7ecae92e6fe3ef72e46
unknown <hspcadmin@HS-VNI432UI.hs.handsome.com.cn>
2021-08-17 23:27:47 +0800

    sb

--- src/utils/sandBox/index.js
+++ src/utils/sandBox/index.js
@@ -3,11 +3,23 @@
  * @Author: caiwu
  * @CreateDate:
  * @LastEditor:
- * @LastEditTime: 2021-08-17 21:29:46
+ * @LastEditTime: 2021-08-17 23:14:39
  */
-
+const reRegExpChar = /[\\^$.*+?()[\]{}|]/g
+const reIsNative = RegExp(`^${
+  Function.prototype.toString.call(Object.prototype.hasOwnProperty)
+    .replace(reRegExpChar, '\\$&')
+    .replace(/hasOwnProperty|(function).*?(?=\\\()| for .+?(?=\\\])/g, '$1.*?')
+}$`)
 const initCode = `const window = this;const self = this;`
 
+function isFunction(value) {
+  const type = typeof value
+  return value != null && type === 'function'
+}
+function isNative(value) {
+  return isFunction(value) && reIsNative.test(value)
+}
 function execCode(src) {
   src = `with (proxyTarget){\n ${initCode}\n${src}\n}`
   return new Function('proxyTarget', src)
@@ -23,16 +35,6 @@ function proxyTarget(target, app, entry, option, hook) {
       hook.call('registerApp', childApp, entry, option)
     },
   }
-  let ignoreList2 = [
-    'setTimeout',
-    'clearTimeout',
-    'setInterval',
-    'requestAnimationFrame',
-    'cancelAnimationFrame',
-    'addEventListener',
-    'getComputedStyle',
-    'postMessage',
-  ]
   let ignoreList = [
     'Object',
     'eval',
@@ -77,7 +79,6 @@ function proxyTarget(target, app, entry, option, hook) {
       return true
     },
     defineProperty: function(target, prop, descriptor) {
-      !descriptor.writable && console.error('禁止子应用在window中定义writable为false的变量;这会造成子应用在第二次打开发生redefine错误')
       return Reflect.defineProperty(window, prop, descriptor)
     },
   })
@@ -86,20 +87,4 @@ function proxyTarget(target, app, entry, option, hook) {
 export default function createSandbox(src, target, app, entry, option, hook) {
   let proxy = proxyTarget(target, app, entry, option, hook)
   execCode(src).call(proxy, proxy)
-}
-
-const reRegExpChar = /[\\^$.*+?()[\]{}|]/g
-
-function isFunction(value) {
-  const type = typeof value
-  return value != null && type === 'function'
-}
-
-const reIsNative = RegExp(`^${
-  Function.prototype.toString.call(Object.prototype.hasOwnProperty)
-    .replace(reRegExpChar, '\\$&')
-    .replace(/hasOwnProperty|(function).*?(?=\\\()| for .+?(?=\\\])/g, '$1.*?')
-}$`)
-function isNative(value) {
-  return isFunction(value) && reIsNative.test(value)
 }
